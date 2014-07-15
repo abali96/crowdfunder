80b596440c2b74903e92bdb9bfc16e61513d0abb
Seven90 <stevesurya@gmail.com>
2014-07-15 16:21:17 -0400

    Fixed sorcery attr

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,19 @@ def new
 end
 
 def create
-  @user = User.new(params[:user])
+  @user = User.new(user_params)
   if @user.save
-    redirect_to root_url, :notice => "Signed up!"
+    redirect_to users_path, :notice => "Signed up!"
   else
     render :new
   end
 end
+
+private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 
 end
 
