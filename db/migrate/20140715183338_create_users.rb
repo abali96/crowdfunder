9e3ac2bb86250cc1164f298f803efa54d11a6719
Seven90 <stevesurya@gmail.com>
2014-07-15 15:39:55 -0400

    Used sorcery gem, and created new users model

--- db/migrate/20140715183338_create_users.rb
+++ db/migrate/20140715183338_create_users.rb
@@ -3,7 +3,6 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
     	t.string :name
     	t.string :email
-    	t.
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140715183751) do
+ActiveRecord::Schema.define(version: 20140715193741) do
 
   create_table "campaigns", force: true do |t|
     t.string   "name"
@@ -40,8 +40,20 @@ ActiveRecord::Schema.define(version: 20140715183751) do
   end
 
   create_table "users", force: true do |t|
+    t.string   "email",                           null: false
+    t.string   "crypted_password",                null: false
+    t.string   "salt",                            null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "remember_me_token"
+    t.datetime "remember_me_token_expires_at"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token"
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token"
 
 end
