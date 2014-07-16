6250b51634f194d752512c5ac5110b1241d0624b
Seven90 <stevesurya@gmail.com>
2014-07-16 01:29:38 -0400

    Fixed js reload after ajax donation bug

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -3,7 +3,6 @@ class RewardsController < ApplicationController
   def pledge
     @reward = Reward.find(params[:id])
     @pledge = @reward.pledges.create (attributes = {user_id: current_user.id})
-    redirect_to campaign_path(@campaign)
   end
 
 end
