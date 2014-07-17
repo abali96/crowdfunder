da59a8fde4e199c061b79c84a939e340c062910b
Arjun <arjun.bali96@gmail.com>
2014-07-16 23:54:28 -0400

    validates if campaign start time is before end time

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -3,4 +3,12 @@ class Campaign < ActiveRecord::Base
   accepts_nested_attributes_for :rewards, :reject_if => :all_blank, :allow_destroy => true
   has_many :users, through: :rewards
   belongs_to :user
+  validate :appropriate_dates
+
+
+  def appropriate_dates
+    if start_date > end_date
+      errors.add(:base, "You can't end your campaign before it starts!")
+    end
+  end
 end
