e004019f3d69e9a9ed41d991089e31650dac2e21
Ashley <ashleykwtam@gmail.com>
2014-07-17 01:16:56 -0400

    created views so you can see your campaigns created, kinda working

--- app/controllers/my/campaigns_controller.rb
+++ app/controllers/my/campaigns_controller.rb
@@ -1,2 +1,40 @@
 class My::CampaignsController < ApplicationController
+  before_filter :require_login
+  before_filter :require_campaign
+
+  def index
+    @campaigns = current_user.campaigns.order('campaigns.created_at DESC').all
+  end
+
+  def new
+    @campaign = current_user.campaigns.build
+  end
+
+  def edit
+  end
+
+  def update
+    if @campaign.update_attributes(params[:campaign])
+      redirect_to [:my, :campaigns], notice: "Campaign updated!"
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @campaign = current_user.campaigns.build(params[:campaign])
+    if @project.save
+      redirect_to [:my, :campaigns], notice: "Campaign created!"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+  end
+
+  private
+  def require_campaign
+    @campaign = current_user.campaigns.all
+  end
 end
