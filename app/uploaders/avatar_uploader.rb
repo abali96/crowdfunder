4721d047ba22c46802ed4a05619318945840e4f5
Husein Kareem <huseinkareem@gmail.com>
2014-07-17 18:15:41 -0400

    removed rmagick

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -3,7 +3,7 @@
 class AvatarUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  include CarrierWave::RMagick
+  #include CarrierWave::RMagick
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
