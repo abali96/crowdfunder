7fa6c8d45b50bd2eb1ae7d81cefef09a12b0bac4
Arjun <arjun.bali96@gmail.com>
2014-07-18 00:44:43 -0400

    comments on user enabled

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,38 @@
 class UsersController < ApplicationController
 
-def new
-  @user = User.new
-end
-
-def create
-  @user = User.new(user_params)
-  if @user.save
-    redirect_to users_path, :notice => "Signed up!"
-  else
-    render :new
+  def show
+    @user = User.find(params[:id])
+    @comments = @user.comments.all
+    @comment = @user.comments.build
   end
-end
 
-def edit
-  @user = User.find(params[:id])
-end
-
-def update
-  @user = User.find(params[:id])
-  if @user.update_attributes(user_params)
-    redirect_to [:my, :campaigns], notice: "User Info Updated!"
-  else
-    render :edit
+  def new
+    @user = User.new
   end
-end
 
-private
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path, :notice => "Signed up!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to [:my, :campaigns], notice: "User Info Updated!"
+    else
+      render :edit
+    end
+  end
+
+  private
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
